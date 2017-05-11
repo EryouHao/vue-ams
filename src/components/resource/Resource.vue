1519336c24f33f35205610fb904e07f76ccc4274
EryouHao <haoeryou@qq.com>
2017-05-11 21:37:18 +0800

    .

--- src/components/resource/Resource.vue
+++ src/components/resource/Resource.vue
@@ -15,6 +15,24 @@
         <el-button type="primary" @click="submit">增加</el-button>
       </el-form-item>
     </el-form>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label=""
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
